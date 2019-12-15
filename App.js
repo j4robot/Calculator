61072b162fab2e48fb24a7446779488734914aa0
codesaitama <achanamosey@gmail.com>
2019-12-15 08:11:03 +0000

    Changes made. Looking at ahndling state. Eg. Getting the input data from the username and password fields. Uncontrolled components style

--- App.js
+++ App.js
@@ -14,101 +14,58 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  Button
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends React.Component{
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Achana</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits...
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  constructor(){
+    super()
+    this.state = {username: '', password: ''}
+    this.btnLoginPressed = this.btnLoginPressed.bind(this)
+  }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  btnLoginPressed = () => {
+    const username = this._username.lastNativeText;
+    const password = this._password.lastNativeText;
+    this.setState({username, password})
+  }
+
+
+  render(){
+    return(
+      <View style={style.container}>
+      <Text>{this.state.username}</Text>
+      <Text>{this.state.password}</Text>
+      <View>
+        <Text style={style.label}>User Name</Text>
+        <TextInput
+        style={style.input} ref={input => this._username = input}></TextInput>
+      </View>
+
+      <View>
+        <Text style={style.label}>Password</Text>
+        <TextInput
+        style={style.input} ref={input => this._password = input}></TextInput>
+      </View>
+      <Button title={'Login'} onPressed={this.btnLoginPressed}></Button>
+      </View>
+      )
+  }
+}
+
+const style = StyleSheet.create({
+  container:{
+    flex: 1, justifyContent: 'center', padding: 20
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  input: {
+    height: 40, borderColor: 'gray', borderWidth: 1
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  label:{
+    fontSize: 17,
+    color: '#212121'
+  }
+  })
 
 export default App;
